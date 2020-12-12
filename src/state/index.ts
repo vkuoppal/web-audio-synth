b16057083fede3301618130f692f24f1d88969a7
Ville Kuoppala <ville.kuoppala@futurice.com>
2020-12-12 12:14:13 +0200

    Update dependencies

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { applicationSlice } from "./slice";
+import { enableMapSet } from "immer";
+
+enableMapSet();
 
 const store = configureStore({
   reducer: applicationSlice.reducer,
-  middleware: [thunk]
-})
+  middleware: [thunk],
+});
 
 export const actions = applicationSlice.actions;
 
